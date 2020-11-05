7aeb21f1687e443ac9e88c3aa94b60b07809042c
shinyeon57 <noeys57@sookmyung.ac.kr>
2020-11-05 16:26:15 +0900

    practice 06

--- main.c
+++ main.c
@@ -3,19 +3,27 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void swap(int *x, int *y){
+int main(void)
+{
+	int i;
+	int grade[5];
+	int sum = 0;
+	int *ptr;		//ptr 
 	
-	int temp;
-	
-	temp = *x;
-	*x = *y;
-	*y = temp;
-
+	for(i=0;i<5;i++)
+	{
+		ptr = &grade[i];		//ith address
+		printf("grade[%i]=", i);
+		scanf("%d", ptr);
+	}
+		
+	for(i=0;i<5;i++)
+	{
+		ptr = &grade[i];		//ptr point array's address
+		sum += *ptr ;			
+		printf("grade[%d]=%d\n", i, *ptr);
 }
 
-void main(void){
-	int a = 3;
-	int b = 5;
-	swap(&a, &b);
-	printf("a:%i, b:%i\n", a, b);
+	printf("average : %i\n", sum/5);
+	return 0;	
 }
